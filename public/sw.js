56cd3e9fab50e5246989e252b22935913710fdbd
carvalho832-glitch <carvalho832@gmail.com>
2026-07-13 19:21:15 -0300

    atualiza cache para leitor QR ao vivo

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quimstock-v3';
+const CACHE_NAME = 'quimstock-v4';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
