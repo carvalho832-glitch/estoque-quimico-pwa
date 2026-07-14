75405bc9729ed25733d9f32c8bc536225509c316
carvalho832-glitch <carvalho832@gmail.com>
2026-07-13 21:34:07 -0300

    atualiza cache para painel desktop

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quimstock-v8';
+const CACHE_NAME = 'quimstock-v9';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
