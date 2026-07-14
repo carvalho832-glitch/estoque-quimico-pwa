8385ba3877b123439fa8187e8e83abc3a1e9c5b9
carvalho832-glitch <carvalho832@gmail.com>
2026-07-13 22:06:08 -0300

    atualiza cache para conexão Firebase

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quimstock-v10';
+const CACHE_NAME = 'quimstock-v11';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
