52db24422db30864462272d7ad66ef372a19c2a7
carvalho832-glitch <carvalho832@gmail.com>
2026-07-23 23:53:44 -0300

    Atualizar cache para modais de produto e PDF

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quimstock-v30';
+const CACHE_NAME = 'quimstock-v31';
 const APP_SHELL = ['./', './index.html', './cloud.html', './manifest.webmanifest', './icon.svg', './facc-logo.svg'];
 
 self.addEventListener('install', (event) => {
