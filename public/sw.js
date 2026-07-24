bf50815242b76df1b08cd442a420082da92bf0c4
carvalho832-glitch <carvalho832@gmail.com>
2026-07-24 01:08:19 -0300

    Atualizar cache após correção da edição

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quimstock-v33';
+const CACHE_NAME = 'quimstock-v34';
 const APP_SHELL = ['./', './index.html', './cloud.html', './manifest.webmanifest', './icon.svg', './facc-logo.svg'];
 
 self.addEventListener('install', (event) => {
