2a5bcde4cb323e6661fad030efc1173493562dc6
carvalho832-glitch <carvalho832@gmail.com>
2026-07-25 16:08:20 -0300

    Atualizar cache para relógio meteorológico

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quimstock-v34';
+const CACHE_NAME = 'quimstock-v35';
 const APP_SHELL = ['./', './index.html', './cloud.html', './manifest.webmanifest', './icon.svg', './facc-logo.svg'];
 
 self.addEventListener('install', (event) => {
