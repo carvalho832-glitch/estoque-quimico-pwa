c4cfd1c940f9525e0912d0ad5f7a0f83d51f7489
carvalho832-glitch <carvalho832@gmail.com>
2026-07-25 16:20:13 -0300

    Atualizar cache para painel climático compacto

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quimstock-v36';
+const CACHE_NAME = 'quimstock-v37';
 const APP_SHELL = ['./', './index.html', './cloud.html', './manifest.webmanifest', './icon.svg', './facc-logo.svg'];
 
 self.addEventListener('install', (event) => {
