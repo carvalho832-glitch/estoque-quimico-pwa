82419603d75d3ead25c4e383bf05cc095eb9fea8
carvalho832-glitch <carvalho832@gmail.com>
2026-07-25 16:27:27 -0300

    Atualizar cache para intervalo climático de 30 minutos

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quimstock-v37';
+const CACHE_NAME = 'quimstock-v38';
 const APP_SHELL = ['./', './index.html', './cloud.html', './manifest.webmanifest', './icon.svg', './facc-logo.svg'];
 
 self.addEventListener('install', (event) => {
