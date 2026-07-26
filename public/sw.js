e92cce5c74ac5015d0f12b977e9e05e233db803d
carvalho832-glitch <carvalho832@gmail.com>
2026-07-26 09:44:01 -0300

    Atualiza cache para ficha técnica FRS-30

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quimstock-v39';
+const CACHE_NAME = 'quimstock-v40';
 const APP_SHELL = ['./', './index.html', './cloud.html', './manifest.webmanifest', './icon.svg', './facc-logo.svg'];
 
 self.addEventListener('install', (event) => {
