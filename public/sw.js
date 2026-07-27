8369b6105a7af51618e9b7fd568e23463613b799
carvalho832-glitch <carvalho832@gmail.com>
2026-07-27 19:21:42 -0300

    Atualizar cache para inventário QR contínuo

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quimstock-v41';
+const CACHE_NAME = 'quimstock-v42';
 const APP_SHELL = ['./', './index.html', './cloud.html', './manifest.webmanifest', './icon.svg', './facc-logo.svg'];
 
 self.addEventListener('install', (event) => {
