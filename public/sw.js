67f4599e1b0f05a247aacb2e20076419480a1e5b
carvalho832-glitch <carvalho832@gmail.com>
2026-07-27 19:43:01 -0300

    Atualizar cache para inventário em lote

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quimstock-v43';
+const CACHE_NAME = 'quimstock-v44';
 const APP_SHELL = ['./', './index.html', './cloud.html', './manifest.webmanifest', './icon.svg', './facc-logo.svg'];
 
 self.addEventListener('install', (event) => {
