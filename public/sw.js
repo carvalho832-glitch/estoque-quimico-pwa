dd694a2ce0004dc14137d029787fe920c2ad5371
carvalho832-glitch <carvalho832@gmail.com>
2026-08-25 16:02:40 -0300

    Atualizar cache para reinício administrativo

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quimstock-v50';
+const CACHE_NAME = 'quimstock-v51';
 const APP_SHELL = ['./', './index.html', './cloud.html', './manifest.webmanifest', './icon.svg', './facc-logo.svg'];
 
 self.addEventListener('install', (event) => {
