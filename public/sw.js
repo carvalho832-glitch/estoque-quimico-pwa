9aa0f378f117e253e951c7f0ec6e432c779ce06f
carvalho832-glitch <carvalho832@gmail.com>
2026-09-05 17:47:26 -0300

    chore: atualiza cache do PWA para sincronização v55

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quimstock-v54';
+const CACHE_NAME = 'quimstock-v55';
 const APP_SHELL = ['./', './index.html', './cloud.html', './manifest.webmanifest', './icon.svg', './facc-logo.svg'];
 
 self.addEventListener('install', (event) => {
