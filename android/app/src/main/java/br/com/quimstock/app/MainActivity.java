99d555b41d18e839ccbf761aa3c388f25cbbe2d0
carvalho832-glitch <carvalho832@gmail.com>
2026-09-05 19:32:11 -0300

    fix: inicia Android em modo imersivo de tela cheia

--- android/app/src/main/java/br/com/quimstock/app/MainActivity.java
+++ android/app/src/main/java/br/com/quimstock/app/MainActivity.java
@@ -1,5 +1,42 @@
 package br.com.quimstock.app;
 
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        enableImmersiveMode();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        enableImmersiveMode();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            enableImmersiveMode();
+        }
+    }
+
+    private void enableImmersiveMode() {
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        View decorView = getWindow().getDecorView();
+        WindowInsetsControllerCompat controller = WindowCompat.getInsetsController(getWindow(), decorView);
+        controller.hide(WindowInsetsCompat.Type.systemBars());
+        controller.setSystemBarsBehavior(
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        );
+    }
+}
