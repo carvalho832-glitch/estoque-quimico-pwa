6a9abb617cdc47a3417f972e8a3c3a7a131455fb
carvalho832-glitch <carvalho832@gmail.com>
2026-09-05 19:33:52 -0300

    chore: renova cache PWA para v57

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quimstock-v56';
+const CACHE_NAME = 'quimstock-v57';
 const APP_SHELL = ['./', './index.html', './cloud.html', './manifest.webmanifest', './icon.svg', './facc-logo.svg'];
 
 self.addEventListener('install', (event) => {
